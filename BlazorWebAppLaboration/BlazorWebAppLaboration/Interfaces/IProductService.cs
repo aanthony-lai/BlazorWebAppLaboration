d94842db04853dd703b2c7f309e11eb091ab91fe
Anthony Lai <Anthony.lai96@gmail.com>
2024-02-24 14:02:14 +0100

    Fix a bug that made currencies revert back to initial value when a product was out of stock.

--- BlazorWebAppLaboration/BlazorWebAppLaboration/Interfaces/IProductService.cs
+++ BlazorWebAppLaboration/BlazorWebAppLaboration/Interfaces/IProductService.cs
@@ -5,8 +5,10 @@ namespace BlazorLaboration.Interfaces
 	public interface IProductService
 	{
 		Task<List<Product>> GetProductsAsync();
-		Task<Product> GetProductByIdAsync(int id);
-		Task<bool> SubtractFromStockAsync(Product product);
+		Task<List<Product>> GetProductsAsync(string selectedCurrency);
+        Task<Product> GetProductByIdAsync(int id);
+		Task<Product> GetProductByIdAsync(int id, string selectedCurrency);
+        Task<bool> SubtractFromStockAsync(Product product);
 		Task AddToStockAsync(Product product, int quantity);
 	}
 }
--- BlazorWebAppLaboration/BlazorWebAppLaboration/Services/ProductService.cs
+++ BlazorWebAppLaboration/BlazorWebAppLaboration/Services/ProductService.cs
@@ -7,12 +7,13 @@ namespace BlazorLaboration.Services
 {
 	public class ProductService : IProductService
 	{
-		//private readonly AppDbContext _appDbContext;
 		private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+		private readonly ICurrencyService _currencyService;
 
 		public ProductService(IDbContextFactory<AppDbContext> dbContextFactory, ICurrencyService currencyService) 
 		{
 			_dbContextFactory = dbContextFactory;
+			_currencyService = currencyService;
 		}
 
 		public async Task<List<Product>> GetProductsAsync()
@@ -25,7 +26,23 @@ namespace BlazorLaboration.Services
 			return result;
 		}
 
-		public async Task<Product> GetProductByIdAsync(int id)
+        public async Task<List<Product>> GetProductsAsync(string selectedCurrency)
+        {
+            using var context = await _dbContextFactory.CreateDbContextAsync();
+
+            var result = context.products.ToList()
+                    ?? new List<Product>();
+
+			if (selectedCurrency == _currencyService.InitialCurrency)
+			{
+				return result;
+			}
+
+			result = await _currencyService.ConvertProductsCurrencyAsync(selectedCurrency, result);
+			return result;
+		}
+
+        public async Task<Product> GetProductByIdAsync(int id)
 		{
 			using var context = await _dbContextFactory.CreateDbContextAsync();
 
@@ -35,7 +52,23 @@ namespace BlazorLaboration.Services
 			return result;
 		}
 
-		public async Task<bool> SubtractFromStockAsync(Product product)
+        public async Task<Product> GetProductByIdAsync(int id, string selectedCurrency)
+        {
+            using var context = await _dbContextFactory.CreateDbContextAsync();
+
+            var result = context.products.FirstOrDefault(x => x.Id == id)
+                ?? new Product();
+
+			if(selectedCurrency == _currencyService.InitialCurrency)
+			{
+				return result;
+			}
+
+			result = await _currencyService.ConvertSingleProductCurrencyAsync(selectedCurrency, result);
+            return result;
+        }
+
+        public async Task<bool> SubtractFromStockAsync(Product product)
 		{
 			using var context = await _dbContextFactory.CreateDbContextAsync();
 
